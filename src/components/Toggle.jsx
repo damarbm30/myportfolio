2a03dd3aa88e61f2651556bcfb3193ae325ae904
Damar Buana Murti <damarbm30@gmail.com>
2022-03-18 20:54:11 +0700

    add dark modes

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../context";
 import "./Toggle.css";
 
 const Toggle = () => {
+  const theme = useContext(ThemeContext);
+
+  const handleClick = () => {
+    theme.dispatch({ type: "TOGGLE" });
+  };
   return (
     <div className="toggle">
       <img src="img/sun.png" alt="" className="toggle-icon" />
       <img src="img/moon.png" alt="" className="toggle-icon" />
-      <div className="toggle-button"></div>
+      <div
+        className="toggle-button"
+        onClick={handleClick}
+        style={{ left: theme.state.darkMode ? -1 : 26 }}
+      ></div>
     </div>
   );
 };
